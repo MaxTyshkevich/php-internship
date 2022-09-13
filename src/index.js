30f55bd831c6cdb0bb92918bec6d1c63c68a5503
Max Tyshkevich <2842778@gmail.com>
2022-09-13 17:38:41 +0300

    feat: task 2

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ const maxAvaible = 3;
 let $checkbox = $('input[type="checkbox"]');
 
 $checkbox.each(function () {
-  console.log("this", this);
-
   $(this).on("click", function (e) {
     let checkedlength = $(`input:checkbox:checked`).length;
-    console.log(checkedlength);
 
     return maxAvaible >= checkedlength ? true : e.preventDefault();
   });
 });
-console.log($checkbox);
+
+/* **************** 2 ****************** */
+
+$("a").each(function (index, elem) {
+  console.dir(elem);
+  if (location.hostname !== elem.hostname || elem.hostname.length) {
+    $(elem).attr("target", "_blank");
+  }
+});
 
 /* **************** 3 ****************** */
 
@@ -31,14 +36,20 @@ $("#input-4").on("input", showCountSumb);
 
 function showCountSumb(e) {
   let currentCount = $(this).val().length;
+  let maxLength = $(this).data("maxlength");
+  let diff = maxLength - currentCount;
 
-  console.log(this.maxLength);
-  $("#show-4").html(`осталось : ${this.maxLength - currentCount} `);
+  let result =
+    diff === 0
+      ? ""
+      : diff > 0
+      ? `осталось : ${maxLength - currentCount} символов`
+      : `перешли ${currentCount - maxLength} символов`;
+
+  $("#show-4").html(result);
 }
 
 /* **************** 5 ****************** */
-
-let colors = ["#3454", "#3454", "#3454"];
 
 let text = $("#task-5").text();
 
