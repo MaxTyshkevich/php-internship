b89c284a8159a78cd7da65ffcfab44e85a0d03dc
Max Tyshkevich <2842778@gmail.com>
2022-09-21 13:02:54 +0300

    feat: sql requiest getAll, remove

--- classes/City.php
+++ classes/City.php
@@ -1,22 +1,35 @@
 <?php
-namespace classes;
-require_once './Db.php';
 
+namespace classes;
+
+require_once 'Db.php';
+
+// require_once './Db.php';  ???
 
 use classes\Db;
 
 
-
-
 class City extends Db
 {
+	public function getAllCity()
+	{
+		$sql = "SELECT * FROM `city`";
 
-	public function ss() {
-		print_r($this->connect);
+		return $this->connect->query($sql);
 	}
 
+	public function deleteCity($id)
+	{
+
+		$sql = "DELETE FROM `city` WHERE `id` = (?)";
+		$stmt = $this->connect->prepare($sql);
+		$stmt->bind_param('i', $id);
+		$stmt->execute();
+
+		return $stmt->get_result();
+	}
 
 }
 
-$city = new City();
-$city->ss();
+//$cityInstance = new City(); где создовать этот obj ??
+//$cityInstance->deleteCity(8);
--- classes/User.php
+++ classes/User.php
@@ -1,80 +1,47 @@
 <?php
+
 namespace classes;
 
 require_once './Db.php';
-use classes\Db;
 
+use classes\Db;
 
 
 class User extends Db
 {
 	private $dataUsers;
 
-	public function getAllUser() {
-			$sql = "SELECT * FROM `user`";
+	public function getAllUser()
+	{
+		$sql = "SELECT * FROM `user`";
 
-			$this->dataUsers = $this->connect->query($sql);
-
-	}
-
-	public function addNewUser() {
+		$this->dataUsers = $this->connect->query($sql);
 
 	}
 
-	public function changeUser($id) {
+	public function addNewUser()
+	{
 
 	}
 
-	public function removeUser($id) {
+	public function changeUser($id)
+	{
 
 	}
 
-	public function renderUser () {
-		foreach ($this->dataUsers as $key => $user) {
-			$template = '<div class="users">
-                    <img width="100" src="/pictures/3921.jpg" class="image" alt="Фотография">
-                    <div class="userdan">
-                        <h4>' . $user['surname'] . ' ' .  $user['name'] . '</h4>
-                        <p>Город:' . $user['city_id'] . '</p>
+	public function removeUser($id)
+	{
+		$sql = "DELETE FROM `city` WHERE `id` = (?)";
+		$stmt = $this->connect->prepare($sql);
+		$stmt->bind_param('i', $id);
+		$stmt->execute();
 
-
-                        <form action="../ajax/action.user.php" method="post">
-                            <input type="hidden" name="id" value="'.$user['id'] .'">
-                            <input type="submit" name="del_fors_names" value="Удалить" onclick="return confirm('Вы действительно хотите удалить пользователя?')">
-                        </form>
-
-                        <form action="" method="post">
-                            <input type="hidden" name="id15" value="44">
-                            <input type="submit" name="edit_fors_names" value="Редактировать">
-                        </form>
-                    </div>
-                </div>';
-		}
-
-// Array ( [id] => 1 [name] => Max [surname] => Tyshkevich [avatar] => [city_id] => 1 )
-//		$template = '<div class="users">
-//                    <img width="100" src="/pictures/3921.jpg" class="image" alt="Фотография">
-//                    <div class="userdan">
-//                        <h4>Михайлов Сергей</h4>
-//                        <p>Город: </p>
-//
-//
-//                        <form action="" method="post">
-//                            <input type="hidden" name="id" value="44">
-//                            <input type="submit" name="del_fors_names" value="Удалить" onclick="return confirm('Вы действительно хотите удалить пользователя?')">
-//                        </form>
-//
-//                        <form action="" method="post">
-//                            <input type="hidden" name="id15" value="44">
-//                            <input type="submit" name="edit_fors_names" value="Редактировать">
-//                        </form>
-//                    </div>
-//                </div>';
-
+		return $stmt->get_result();
 	}
 
 
 }
+
 
 $test = new User();
 $test->getAllUser();
