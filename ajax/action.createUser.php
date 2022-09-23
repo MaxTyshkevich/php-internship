8295a09dd80eca8b802e7b3ee4188f0e405f126d
Max Tyshkevich <2842778@gmail.com>
2022-09-23 13:08:43 +0300

    feat: add fn saveAvatarUser

--- ajax/action.createUser.php
+++ ajax/action.createUser.php
@@ -1,1 +1,14 @@
 <?php
+
+function saveAvatarUser($img)
+{
+	move_uploaded_file($img['tmp_name'], 'pictures/' . $img['name']);
+}
+
+
+
+if (isset($_FILES['uploadfile'])) {
+	saveAvatarUser($_FILES['uploadfile']);
+} else {
+	// нужно вернуть текст ошибки
+}
