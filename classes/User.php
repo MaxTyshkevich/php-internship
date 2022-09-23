2a5c5ea053dce56c52e11ed96c8cb1ee7fdb4542
Max Tyshkevich <2842778@gmail.com>
2022-09-23 13:07:35 +0300

    feat: add fn getUser

--- classes/User.php
+++ classes/User.php
@@ -9,22 +9,46 @@ use classes\Db;
 
 class User extends Db
 {
+	public function getUser($id)
+	{
+		$sql = "SELECT * FROM `user` WHERE `id` = ?";
+		$stmt = $this->connect->prepare($sql);
+
+		$stmt->bind_param('i', $id);
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+		return $result->fetch_assoc();
+	}
 
 	public function getAllUser()
 	{
 		$sql = "SELECT * FROM `user`";
+		$stmt = $this->connect->prepare($sql);
 
-		return $this->connect->query($sql);
+		$stmt->execute();
+
+		return $stmt->get_result();
 	}
 
-	public function addNewUser()
+	public function addNewUser($name, $surname, $avatar, $city)
 	{
+		$sql = "INSERT INTO `user` (`name`, `surname`, `avatar`, `city_id`) VALUES ( ?, ?, ?, ?)";
+		$stmt = $this->connect->prepare($sql);
+		$stmt->bind_param('sssi', $name, $surname, $avatar, $city);
+		$stmt->execute();
 
+		return $stmt->get_result(); // ничего не возвращает
 	}
 
 	public function changeUser($id)
 	{
+		/*$sql = "INSERT INTO `user` (`name`, `surname`, `avatar`, `city_id`) VALUES ( ?, ?, ?, ?)";
+		$stmt = $this->connect->prepare($sql);
+		$stmt->bind_param('sssi', $name, $surname, $avatar, $city);
+		$stmt->execute();
 
+		return $stmt->get_result(); // ничего не возвращает*/
 	}
 
 	public function removeUser($id)
@@ -34,10 +58,12 @@ class User extends Db
 		$stmt->bind_param('i', $id);
 		$stmt->execute();
 
-		return $stmt->get_result();
+		return $stmt->get_result();  // ничего не возвращает
 	}
 
 
 }
 
 
+//$s = new User();
+//print_r($s->getAllUser());
